633efd162e201dfa8435142b1070c8a260009565
Lucas Phillips <lphill57@vols.utk.edu>
2024-04-26 07:34:18 -0400

    Update logo and links

--- pages/2_🪟_Split_Map.py
+++ pages/2_🪟_Split_Map.py
@@ -5,7 +5,7 @@ st.set_page_config(layout="wide")
 
 markdown = """
 A Streamlit map template
-<https://github.com/opengeos/streamlit-map-template>
+<https://github.com/phillipslucas/Streamlit_Temp>
 """
 
 st.sidebar.title("About")
